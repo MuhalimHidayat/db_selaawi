2481edc0f790360259596ea73a4c8388d23bc85e
MuhalimHidayat <hidayatalim27@gmail.com>
2024-01-31 15:05:47 +0700

    add dataset completed

--- app/blueprints/land_predict/land_predict.py
+++ app/blueprints/land_predict/land_predict.py
@@ -6,9 +6,16 @@ from flask import (
 from markupsafe import escape
 import pandas as pd
 from werkzeug.security import check_password_hash, generate_password_hash
-from app import model
+from werkzeug.utils import secure_filename
+from app import model, app
+
+
+UPLOAD_FOLDER = 'app/blueprints/land_predict/static/datasets'
+ALLOWED_EXTENSIONS = {'xlsx','csv'}
+
 # masih salah di bagian static_url_path
 lp = Blueprint('land_predict', __name__, url_prefix='/land_predict', template_folder='templates', static_folder='static', static_url_path='/static/land_predict')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @lp.route('/add-manual-data', methods=('GET', 'POST'))
 def add_manual_data():
@@ -26,7 +33,40 @@ def add_manual_data():
         data_test = pd.DataFrame(data, columns=columns)
         prediction = model.predict(data_test)
         print("PREDIKSI: ",prediction)
+        # nilai akurasinya 
+        
         # print(humidity)
         flash("data berhasil di prediksi", "success")
         return render_template('add_manual_data.html', prediction=prediction)
     return render_template('add_manual_data.html', prediction="Belum Memasukkan Data")
+
+# dataset upload
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@lp.route('/add-dataset', methods=('GET', 'POST'))
+def add_dataset():
+    if request.method == 'POST':
+        if 'dataset' not in request.files:
+            flash('No file part', "danger")
+            return redirect(request.url)
+        file = request.files['dataset']
+        if file.filename == '':
+            flash('No selected file', "danger")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            # MACHINE LEARNING MODEL HERE
+            df = pd.read_excel(request.files['dataset'])
+            X = df.iloc[:, :-1]
+            prediction = model.predict(X)
+            df['prediction'] = prediction
+            # df.to_excel(UPLOAD_FOLDER+'/'+filename)
+            df.to_excel('app/blueprints/land_predict/static/datasets/'+filename, index=False)
+            # file.save(UPLOAD_FOLDER+'/'+filename)
+            flash('File berhasil di upload', "success")
+            return render_template('add_dataset.html', prediction="berhasil di prediksi")
+        return render_template('add_dataset.html')
+    return render_template('add_dataset.html')
+    

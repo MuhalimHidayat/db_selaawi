754dc964f7fcecce97d57c08394a4fb302a92f32
MuhalimHidayat <hidayatalim27@gmail.com>
2024-02-19 12:07:50 +0700

    data_sample frontend

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,7 @@ def index():
 @app.route('/tabel-result')
 def tabel_result():
     return render_template('user/tabel_result.html')
+
+@app.route('/data-sample')
+def data_sample():
+    return render_template('user/data_sample.html')

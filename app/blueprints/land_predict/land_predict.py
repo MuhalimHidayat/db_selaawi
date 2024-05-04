b3a4494f3087b4bad92c29ab01ddcc162e39bfc2
MuhalimHidayat <hidayatalim27@gmail.com>
2024-05-04 16:09:21 +0700

    fixing conflict

--- app/blueprints/land_predict/land_predict.py
+++ app/blueprints/land_predict/land_predict.py
@@ -24,6 +24,12 @@ lp = Blueprint('land_predict', __name__, url_prefix='/land_predict', static_fold
 # mengambil seluruh yang ada pada file add_manual_data
 from app.blueprints.land_predict import add_manual_data
 
+@lp.route('/dashboard')
+def dashboard():
+    if 'id' not in session:
+        flash('You must be logged in to access this page','danger')
+        return redirect(url_for('auth.sign_in'))
+    return render_template('pre_content/dashboard.html')
 
 # dataset upload
 def allowed_file(filename):

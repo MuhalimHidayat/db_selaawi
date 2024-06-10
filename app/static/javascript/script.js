8c96906510c89cb908e9c79f03d85f8879e787ca
MuhalimHidayat <hidayatalim27@gmail.com>
2024-06-10 13:58:45 +0700

    style

--- app/static/javascript/script.js
+++ app/static/javascript/script.js
@@ -59,3 +59,28 @@ btnNav.addEventListener("click", (e) => {
 }
 );
 
+// sidebar mark
+function getCurrentURL() {
+    return window.location.href;
+}
+
+const url = getCurrentURL();
+console.log(url);
+
+// http://127.0.0.1:5000/land_predict/result-manual-data/dt/%5B%7B%22area%22:%22Ciawi%22,%22hum%22:55.4,%22soil_nitro1%22:22.0,%22soil_phos1%22:98.0,%22soil_pot1%22:90.0,%22soil_temp1%22:25.3,%22soil_ph1%22:5.5,%22temp%22:29.7,%22id_m%22:39%7D,%7B%22area%22:%22Rajapolah%22,%22hum%22:56.2,%22soil_nitro1%22:22.0,%22soil_phos1%22:98.0,%22soil_pot1%22:91.0,%22soil_temp1%22:25.4,%22soil_ph1%22:5.6,%22temp%22:29.3,%22id_m%22:38%7D,%7B%22area%22:%22Nanggeleng%22,%22hum%22:54.9,%22soil_nitro1%22:22.0,%22soil_phos1%22:98.0,%22soil_pot1%22:90.0,%22soil_temp1%22:25.3,%22soil_ph1%22:5.6,%22temp%22:29.6,%22id_m%22:37%7D,%7B%22area%22:%22Kolab%22,%22hum%22:60.0,%22soil_nitro1%22:200.0,%22soil_phos1%22:90.0,%22soil_pot1%22:100.0,%22soil_temp1%22:28.0,%22soil_ph1%22:5.7,%22temp%22:28.0,%22id_m%22:36%7D,%7B%22area%22:%22Warming%20up%22,%22hum%22:60.0,%22soil_nitro1%22:200.0,%22soil_phos1%22:90.0,%22soil_pot1%22:100.0,%22soil_temp1%22:28.0,%22soil_ph1%22:5.7,%22temp%22:28.0,%22id_m%22:35%7D,%7B%22area%22:%22Nanggeleng%22,%22hum%22:60.0,%22soil_nitro1%22:200.0,%22soil_phos1%22:98.0,%22soil_pot1%22:100.0,%22soil_temp1%22:28.0,%22soil_ph1%22:5.7,%22temp%22:28.0,%22id_m%22:33%7D,%7B%22area%22:%22Nagrog%22,%22hum%22:56.8,%22soil_nitro1%22:22.0,%22soil_phos1%22:97.0,%22soil_pot1%22:90.0,%22soil_temp1%22:25.1,%22soil_ph1%22:5.6,%22temp%22:29.1,%22id_m%22:32%7D,%7B%22area%22:%22Telkom%20University%22,%22hum%22:60.0,%22soil_nitro1%22:200.0,%22soil_phos1%22:90.0,%22soil_pot1%22:100.0,%22soil_temp1%22:28.0,%22soil_ph1%22:5.8,%22temp%22:28.0,%22id_m%22:31%7D%5D
+// itu url yang didapatkan, jika didalam url terdapat kata "manual-data" maka sidebar akan menampilkan mark pada menu "Manual Data"
+if (url.includes("manual-data")) {
+    path = document.getElementById("manual-data").classList.add("mark");
+    console.log(path)
+} else if (url.includes("dataset")) {
+    path = document.getElementById("dataset").classList.add("mark");
+    console.log(path)
+} else if (url.includes("dashboard")) {
+    path = document.getElementById("dashboard").classList.add("mark");
+    console.log(path)
+} else if (url.includes("real-time")) {
+    path = document.getElementById("real-time").classList.add("mark");
+    console.log(path)
+} else{
+    console.log("notfound")
+}

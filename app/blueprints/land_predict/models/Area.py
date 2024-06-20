c4d977d23d21812d17e3f08fac3857209af5c1b1
MuhalimHidayat <hidayatalim27@gmail.com>
2024-06-20 15:58:33 +0700

    feat:new foreign key on Area

--- app/blueprints/land_predict/models/Area.py
+++ app/blueprints/land_predict/models/Area.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, ForeignKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship, backref
 from sqlalchemy.sql import func
 from app import db, app
@@ -13,5 +13,20 @@ class Area(db.Model):
     created_at = db.Column(db.String(250), nullable=False, default=func.now())
     updated_at = db.Column(db.String(250), nullable=False, default=func.now())  
     id = db.Column(db.Integer, ForeignKey('manualdata.id_m'))
+    id_d = db.Column(db.Integer, ForeignKey('dataset.id_d'))
     
-    manualdata = relationship('ManualData', backref=backref('area_backref', uselist=True))
+    manualdata = relationship('ManualData', backref=backref('areas', uselist=True))
+    # dataset = relationship('Dataset', backref=backref('areas', uselist=True))
+    dataset = relationship('Dataset', backref=backref('areas', uselist=True))
+    
+    __table_args__ = (
+        db.ForeignKeyConstraint(
+            ['id'], ['manualdata.id_m'],
+            name='fk_area_manualdata'
+        ),
+        db.ForeignKeyConstraint(
+            ['id_d'], ['dataset.id_d'],
+            name='fk_area_dataset'
+        )
+    )
+    

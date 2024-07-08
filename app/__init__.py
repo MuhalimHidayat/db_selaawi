6ff8673a49a986389a73643c3ae583e87fb37045
MuhalimHidayat <hidayatalim27@gmail.com>
2024-07-08 08:00:38 +0700

    add instance _relative_config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import joblib
 import psycopg2
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://selaawi_owner:kS5TYUyR0dcN@ep-still-recipe-a1p69oue.ap-southeast-1.aws.neon.tech/selaawi?sslmode=require"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False

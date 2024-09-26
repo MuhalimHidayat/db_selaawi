ec07ce68f1b40ba0ae8c9436e66afbe69900d2ac
MuhalimHidayat <hidayatalim27@gmail.com>
2024-09-26 08:01:17 +0700

    hosting

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ import psycopg2
 from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__, instance_relative_config=True)
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://selaawi_owner:kS5TYUyR0dcN@ep-still-recipe-a1p69oue.ap-southeast-1.aws.neon.tech/selaawi?sslmode=require"
 # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://selaawi_owner:endpoint=ep-still-recipe-a1p69oue;kS5TYUyR0dcN@ep-still-recipe-a1p69oue.ap-southeast-1.aws.neon.tech/selaawi?sslmode=require"
 
@@ -24,7 +24,7 @@ model_dt = joblib.load('app/blueprints/land_predict/static/ml_model/DT_model.sav
 
 from app.blueprints.land_predict.land_predict import lp
 app.register_blueprint(lp)
-app.wsgi_app = ProxyFix(app.wsgi_app)
+
 
 @app.route('/download-dataset/<dataset_name>')
 def download_dataset(dataset_name):
